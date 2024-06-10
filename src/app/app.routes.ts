2a5b2914b4547b4ad0871e475d707b9901f0564f
rehanalam9141 <rehanalam9141@gmail.com>
2024-06-10 21:02:50 +0500

    themes customized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'products', component:ProductsComponent },
+    { path:'checkout', component:CheckoutComponent }
+];
